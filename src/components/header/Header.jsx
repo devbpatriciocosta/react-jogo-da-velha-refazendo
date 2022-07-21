c3a9a636239ec1bc692d89effb1b6ea1652386e3
Bruno Patrício <bpatriciocosta@gmail.com>
2022-07-21 13:20:54 -0300

    Criado componente ICON

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from './Header.module.css'
 
 import Subtitle from '../Subtitle/Subtitle'
 import Title from '../title/Title'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
-    <Title> Jogo da velha - Tik Tak Toe </Title>
-    <Subtitle> Criado por Bruno Patrício - @devbpatriciocosta</Subtitle>
+      <Title> Jogo da velha - Tik Tak Toe </Title>
+      <Subtitle> Criado por Bruno Patrício - @devbpatriciocosta</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/devbpatriciocosta"/>
+      </div>
     </div>
   )
 }
